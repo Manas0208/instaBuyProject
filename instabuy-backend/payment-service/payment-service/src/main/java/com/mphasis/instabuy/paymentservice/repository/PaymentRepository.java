e32ca55560c556162717ed0adc62d6f49a9c1b9e
Manas0208 <2023aspire75@gmail.com>
2026-04-22 16:16:52 +0530

    Delete instabuy-backend directory

--- instabuy-backend/payment-service/payment-service/src/main/java/com/mphasis/instabuy/paymentservice/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mphasis.instabuy.paymentservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mphasis.instabuy.paymentservice.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
